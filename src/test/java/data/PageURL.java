55919998f545407fc3a720d1220f5a326996a8cf
CloverLii <clover.shufen.li@outlook.com>
2021-02-16 00:36:01 +1300

    Add more element and functions in [Login] Page

---
+++ src/test/java/data/PageURL.java
@@ -0,0 +1,17 @@
+package data;
+
+/**
+ * List of page url for assertion
+ * @author cloverli
+ * @date 15/02/2012
+ *
+ */
+public class PageURL {
+	
+	public static String LOGIN_PAGE = "https://secure.swipedon.com/login";
+	
+	public static String REGISTER_PAGE = "https://www.swipedon.com/register";
+	
+	public static String FORGET_PASSWORD_PAGE = "https://secure.swipedon.com/forgot";
+	
+}
---
+++ src/test/java/pom/pages/ForgetPasswordPage.java
@@ -0,0 +1,14 @@
+package pom.pages;
+
+import org.openqa.selenium.WebDriver;
+
+import base.BasePage;
+
+public class ForgetPasswordPage extends BasePage{
+
+	public ForgetPasswordPage(WebDriver driver) {
+		super(driver);
+		// TODO Auto-generated constructor stub
+	}
+
+}
